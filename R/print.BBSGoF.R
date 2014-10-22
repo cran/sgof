7e338e69d3a266292f553b89723cdf20f6d9d7b4
Irene Castro Conde <irene.castro@uvigo.es>
2014-10-22 00:00:00 +0000

    version 2.1.1

--- R/print.BBSGoF.R
+++ R/print.BBSGoF.R
@@ -13,10 +13,10 @@ cat("\n")
 
 if(length(x$deleted.blocks)>1){
 cat("Warning:","\n")
-cat("Blocks", sort(x$deleted.blocks), "have been removed because they provided negative or atypical variances.","\n")
+cat("Blocks", sort(x$deleted.blocks), "have been removed because they provided negative variances.","\n")
 }else if(length(x$deleted.blocks)==1&is.na(x$deleted.blocks)==F&is.infinite(x$deleted.blocks)==F){
 cat("Warning:","\n")
-cat("Block", sort(x$deleted.blocks), "has been removed because it provided negative or atypical variance.","\n")
+cat("Block", sort(x$deleted.blocks), "has been removed because it provided negative variance.","\n")
 }
 
 cat("\n")
